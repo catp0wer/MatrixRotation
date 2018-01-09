cf159979373726e2f3e943dd91b0078f8ee41060
catp0wer <catp0wer@mail.ru>
2018-01-09 22:36:30 +0200

    the skeleton of the program

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        int[][] my_array = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+
+
+    }
+}
